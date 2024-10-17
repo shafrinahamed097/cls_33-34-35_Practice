af3886181c3beb1cc2ea9e264551607753a165ee
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-10-17 15:29:27 +0600

    Generate User Registration & Login Function & Install JWTToken

---
+++ app/Helper/JWTToken.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Helper;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JWTToken{
+   public static function CreateToken($userEmail):string{
+        $key=env('JWT_KEY');
+        $payload=[
+            'iss'=>'laravel-token',
+            'iat'=>time(),
+            'exp'=>time()+60*5,
+            'userEmail'=>$userEmail
+        ];
+
+     return $jwt = JWT::encode($payload, $key, 'HS256');
+        
+    }
+
+  public static  function VerifyToken($token):string{
+        try{
+            $key=env('JWT_KEY');
+        $decoded=JWT::decode($token, new Key($key, 'HS256'));
+        return $decoded->userEmail;
+
+        }
+        catch(\Exception ){
+            return 'unauthorized';
+        }
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helper\JWTToken;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    // User Registration
+    function UserRegistration(Request $request)
+    {
+      try{
+        User::create([
+         'firstName'=>$request->input('firstName'),
+         'lastName'=>$request->input('lastName'),
+         'email'=>$request->input('email'), 
+         'mobile'=>$request->input('mobile'),
+         'password'=>$request->input('password'),
+          
+      ]);
+      return response()->json([
+        'status'=> 'success',
+        'message' => 'User Registered Successfully',
+      ],200);
+      }
+      catch(\Exception $e){
+        return response()->json([
+          'status'=> 'error',
+          'message' => 'User Registration Failed',
+        ],500); 
+      }
+    }
+
+    // User Login
+    function UserLogin(Request $request){
+   $count =  User::where('email', '=',$request->input('email'))
+      ->where('password', '=',$request->input('password'))->count();
+
+      if($count==1){
+        // User Login -> JWT Token Issue
+        $token = JWTToken::CreateToken($request->input('email'));
+        return response()->json([
+          'status'=> 'success',
+          'message' => 'User Login Successfully',
+          'token' => $token,
+        ],200);
+
+      }
+
+      else{
+        return response()->json([
+          'status'=> 'error',
+          'message' => 'User Login Failed',
+        ],401);
+      }
+    }
+}
